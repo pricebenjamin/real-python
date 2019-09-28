f28c144cfcc356bccb76b71eefef0ede383a43ab
Ben Price <price.benjamindaniel@gmail.com>
2019-09-27 20:27:44 -0700

    Rewrite `fetch.py` using Summarizer API; include example in README

---
+++ fetch.py
@@ -0,0 +1,22 @@
+import os
+from operator import attrgetter
+
+# Local imports
+from summarizer import Summarizer
+
+if __name__ == "__main__":
+    s = Summarizer(
+        selected_topics="all", include_premium=True, output_dir="generated_markdown"
+    )
+
+    for topic in s.topics:
+        filename = f"{topic.name} tutorials and courses.md"
+        path = os.path.join(s.output_dir, filename)
+        print(f"\nWriting file `{path}`\n")
+        with open(path, "w") as f:
+            f.write(f"# {topic.name} tutorials and courses from Real Python" + "\n\n")
+            for tutorial in topic.tutorials:
+                f.write(tutorial.markdown_title + "\n\n")
+                if tutorial.has_metadata_string:
+                    f.write(tutorial.markdown_metadata_string + "\n\n")
+                f.write(tutorial.markdown_introduction + "\n\n")
--- test_summarizer.py
+++ test_summarizer.py
@@ -6,18 +6,30 @@ from summarizer import Summarizer
 
 if __name__ == "__main__":
 
-    s = Summarizer(selected_topics="all", include_premium=True, output_dir="test_md")
+    s = Summarizer(
+        selected_topics=["advanced"],
+        include_premium=False,
+        output_dir="advanced_tutorials",
+    )
 
     for topic in s.topics:
-
-        filename = f"{topic.name} tutorials and courses.md"
+        filename = f"{topic.name} tutorials.md"
         path = os.path.join(s.output_dir, filename)
 
-        print(f"    Writing file `{path}`")
-        with open(path, "w") as f:
-            f.write(f"# {topic.name} tutorials and courses from Real Python" + "\n\n")
+        tutorials = sorted(
+            (t for t in topic.tutorials if t.has_comments),  # Filter tutorials
+            key=attrgetter("comments.count"),  # Sort by # comments
+            reverse=True,
+        )
 
-            for tutorial in topic.tutorials:
+        # Check that we've found tutorials matching our criteria
+        assert len(tutorials) > 0
+
+        print(f"\nWriting file `{path}`\n")
+
+        with open(path, "w") as f:
+            f.write(f"# {topic.name} tutorials from Real Python" + "\n\n")
+            for tutorial in tutorials:
                 f.write(tutorial.markdown_title + "\n\n")
                 if tutorial.has_metadata_string:
                     f.write(tutorial.markdown_metadata_string + "\n\n")
